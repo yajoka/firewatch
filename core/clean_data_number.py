ee926b76cddfb4127ecd556fe06606ebd9e572fb
Timmabl <timmaubele@gmx.de>
2025-11-18 15:04:08 +0100

    feature engineering.

--- core/clean_data_number.py
+++ core/clean_data_number.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 file_path = "../data/Number_of_fires_by_month.csv"
 
@@ -17,8 +18,9 @@ df = df[df["Month"].notna()]
 df = df[~df['Month'].str.contains("Unspecified", case=False, na=False)]
 
 
-# Alle leeren Werte (NaN) durch 0 ersetzen
-df = df.fillna(0)
+# Alle leeren Werte durch NaN ersetzen
+df = df.replace(["", " ",], np.nan)
+
 
 # "Wide" → "Long" Format
 df_long = df.melt(
@@ -31,11 +33,36 @@ df_long = df.melt(
 df_long["Year"] = df_long["Year"].astype(int)
 
 # Anzahl (Number_fires) als ganze Zahl
-df_long["Number_fires"] = df_long["Number_fires"].astype(int)
+df_long["Number_fires"] = pd.to_numeric(df_long["Number_fires"], errors="coerce").astype("Int64")
 
 # Jurisdiction als Kategorie, Month als 1–12 umwandeln
 df_long["Jurisdiction"] = df_long["Jurisdiction"].astype("category")
 df_long["Month"] = pd.to_datetime(df_long["Month"], format="%B").dt.month
+
+# Funktion zur Zuweisung der Jahreszeit (Season)
+def get_season(month):
+    # Definitionen: Frühling (März-Mai), Sommer (Juni-August),
+    # Herbst (September-November) und Winter (Dezember-Februar)
+    if month in [3, 4, 5]:
+        return "Spring"
+    elif month in [6, 7, 8]:
+        return "Summer"
+    elif month in [9, 10, 11]:
+        return "Fall"
+    elif month in [12, 1, 2]:
+        return "Winter"
+    return np.nan
+
+# Anwenden der Funktion auf die numerische Monatsspalte
+df_long["Season"] = df_long["Month"].apply(get_season)
+
+# Lag hinufügen, vorherige Werte merken
+df_long = df_long.sort_values(["Jurisdiction", "Year", "Month"])
+
+df_long["Lag_1"] = df_long.groupby("Jurisdiction")["Number_fires"].shift(1)
+df_long["Lag_2"] = df_long.groupby("Jurisdiction")["Number_fires"].shift(2)
+df_long["Lag_3"] = df_long.groupby("Jurisdiction")["Number_fires"].shift(3)
+
 
 #Datei speichern
 output_path = "../data/cleaned_Number_of_fires_by_month.csv"
